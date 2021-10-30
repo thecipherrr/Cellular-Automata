d42bbe8c8c35bc0fdcd670b3e1c0e741faaeff73
Edward Matthew <edward.kurniawan@binus.ac.id>
2021-10-30 11:31:32 +0700

    implemented static grid

--- Life/main.py
+++ Life/main.py
@@ -1,42 +1,44 @@
 import pygame
 import sys
-import random 
+import random
 
-width = 400
-height = 400
-size = 20
+dimensions = (400, 400)
+black = (0,0,0)
+white = (255, 255, 255)
+
+pygame.init()
+display = pygame.display.set_mode((dimensions[0], dimensions[1]))
+display.fill("white")
+pygame.display.set_caption("Conway's Game of Life")
+clock = pygame.time.Clock()
+fps = 60
+
+black = (0,0,0)
+white = (255, 255, 255)
+
 
 def makeGrid():
     grid = []
-    for row in range(width):
+    for row in range(0, dimensions[0]):
         grid.append([])
-        for cols in range(height):
+        for cols in range(0, dimensions[1]):
             grid[row].append(random.randint(0, 1))
+    
     return grid
 
-def drawGrid(grid):
-    size = 10
+grid = makeGrid()
 
-    pygame.init()
-    display = pygame.display.set_mode((width, height))
-    display.fill("white")
+while True:
+    for event in pygame.event.get():
+       if event.type == pygame.QUIT:
+           pygame.quit()
+           sys.exit()
 
-    for row in range(0, width, size):
-        for cols in range(0, height, size):
+    for row in range(0, dimensions[0], 20):
+        for cols in range(0, dimensions[1], 20):
+            rect = pygame.Rect(row, cols, 20, 20)
             if grid[row][cols] == 1:
-                pygame.draw.rect(display, "black", row * width)
+                pygame.draw.rect(display, black, rect)
             else:
-                pygame.draw.rect(display, "black", cols * height, 1)
-
-def main():
-    grid = makeGrid()
-    drawGrid(grid)
-    while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
-        pygame.display.update()
-
-if __name__ == "__main__":
-    main()
+                pygame.draw.rect(display, black, rect, 1)
+    pygame.display.flip()
