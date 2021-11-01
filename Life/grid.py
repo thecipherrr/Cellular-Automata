12861af9f20569df984c646387413e34fc271bac
Edward Matthew <edward.kurniawan@binus.ac.id>
2021-11-02 00:47:35 +0700

    added working OOP Implementation

--- Life/grid.py
+++ Life/grid.py
@@ -4,25 +4,23 @@ import pygame
 
 
 class Grid:
-    def __init__(self, width, height, resolution, size):
-        self.width = width
-        self.height = height
+    def __init__(self, width, height, resolution, margin):
         self.resolution = resolution
-        self.size = size
+        self.margin = margin
 
         # rows and columns of the grid
         # make it scalable
-        self.row = width/resolution
-        self.cols = height/resolution
+        self.row = int(width/resolution)
+        self.cols = int(height/resolution)
         self.size = (self.row, self.cols)
 
         # the grid itself
-        self.grid = np.ndarray(shape=self.size)
+        self.grid_life = np.ndarray(shape=(self.size))
 
     def random2d(self):
-        for i in range(self.width):
-            for j in range(self.height):
-                self.grid[i][j] = random.randint(0,1)
+        for i in range(self.row):
+            for j in range(self.cols):
+                self.grid_life[i][j] = random.randint(0,1)
 
     def neighbours(self, x, y):
         # loop from -1, 2 to check all the neighbours
@@ -30,25 +28,39 @@ class Grid:
         total = 0
         for i in range(-1, 2):
             for j in range(-1, 2):
-                # implement toroid boundary conditions later
-                pass 
-
+                # toroidal boundary conditions
+                x_edge = (x+i+self.row)%self.row
+                y_edge = (y+j+self.cols)%self.cols
+                total += self.grid_life[x_edge][y_edge]
+        
+        total -= self.grid_life[x][y]
+        return total
 
     def gameOfLife(self, aliveColor, deadColor, surface):
-        for i in range(self.width):
-            for j in range(self.height):
+        for i in range(self.row):
+            for j in range(self.cols):
                 x_pos = i * self.resolution
                 y_pos = j * self.resolution
 
-            # draw the live and dead cells
-            if self.grid[x_pos][y_pos] == 1:
-                pygame.draw.rect(surface, aliveColor, [x_pos, y_pos, self.scale-self.size, self.scale-self.size])
-            else:
-                pygame.draw.rect(surface, deadColor, [x_pos, y_pos, self.scale-self.size, self.scale-self.size])
+                # draw the live and dead cells
+                if self.grid_life[i][j] == 1:
+                    pygame.draw.rect(surface, aliveColor, [x_pos, y_pos, self.resolution-self.margin, 
+                    self.resolution-self.margin])
+                else:
+                    pygame.draw.rect(surface, deadColor, [x_pos, y_pos, self.resolution-self.margin,
+                    self.resolution-self.margin])
 
         # generating the next generation
-        nextGrid = np.ndarray(shape=self.size)
+        nextGrid = np.ndarray(shape=(self.size))
         for i in range(self.row):
             for j in range(self.cols):
-                # implement the rules
-                break
+                # get the state of the cell
+                state = self.grid_life[i][j]
+                neighbours = self.neighbours(i, j)
+                if state == 0 and neighbours == 3:
+                    nextGrid[i][j] = 1
+                elif state == 1 and (neighbours < 2 or neighbours > 3):
+                    nextGrid[i][j] = 0
+                else:
+                    nextGrid[i][j] = state
+            self.grid_life = nextGrid
--- Life/main.py
+++ Life/main.py
@@ -1,12 +1,10 @@
 import pygame
-import random
 import os
-import numpy as np 
-
+import grid
 
 os.environ["SDL_VIDEO_CENTERED"] = "1"
 
-width, height = 100, 100 
+width, height = 1366, 768
 size = (width, height)
 black = (0,0,0)
 white = (255, 255, 255)
@@ -15,62 +13,27 @@ pygame.init()
 display = pygame.display.set_mode(size)
 pygame.display.set_caption("Conway's Game of Life")
 clock = pygame.time.Clock()
-fps =  60
-black = (0,0,0)
-white = (255, 255, 255)
-scale = 30
+fps =  30
+resolution = 20
+margin = 1
+running = True
 
-def makeGrid():
-    grid = []
-    for row in range(0, width):
-        grid.append([])
-        for cols in range(0, height):
-            grid[row].append(random.randint(0,1))
-    
-    return grid
-
-
-def checkNeighbours(grid, i, j):
-    total = 0
-    for n in range(-1, 2):
-        for m in range(-1, 2):
-            x = (i+n+len(grid[0])) % len(grid[0])
-            y = (j+m+len(grid[1])) % len(grid[1]) 
-            total += grid[x][y]
-
-    total -= grid[i][j] 
-    return total
-
-
-def main():   
+def main():
+    Grid = grid.Grid(width, height, resolution, margin) 
+    Grid.random2d()
     running = True
-    grid = np.ndarray(shape=size)
-    grid = makeGrid()
+
     while running:
-        display.fill(white)
         clock.tick(fps)
+        display.fill(black) 
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+    
+        Grid.gameOfLife(black, white, display)  
 
-        for row in range(width):
-            for cols in range(height):
-                rect = pygame.Rect(row*10, cols*10, 10, 10)
-                if grid[row][cols] == 1:
-                    pygame.draw.rect(display, black, rect)
-                else:
-                    pygame.draw.rect(display, black, rect, 1)
-        
-        for i in range(len(grid[0])):
-            for j in range(len(grid[1])):
-                # rule checking
-                if grid[i][j] == 0 and checkNeighbours(grid, i, j) == 3:
-                    grid[i][j] = 1
-                elif grid[i][j] == 1 and (checkNeighbours(grid, i, j) < 2 or checkNeighbours(grid, i, j) > 3):
-                    grid[i][j] = 0
-                else:
-                    pass
-        pygame.display.flip()           
+        pygame.display.update()
     pygame.quit()
 
 
